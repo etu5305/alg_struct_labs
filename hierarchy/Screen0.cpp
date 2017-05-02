0cbb997cb42f584e7548f5179275023897ba071c
Pavel Bilkis <p.bilkis@gmail.com>
2017-05-03 02:04:24 +0300

    code was cleaned add report final

--- hierarchy/Screen0.cpp
+++ hierarchy/Screen0.cpp
@@ -1,7 +1,5 @@
 //Прикладная программа: пополнение и использование библиотеки фигур
 #include <iostream>
-#include <clocale>
-#include <exception>
 using namespace std;
 #include "screen.h"
 #include "shape.h"
@@ -18,58 +16,49 @@ public:
 };
 myshape::myshape(point a, point b) : rectangle(a,b)
 {
-	int ll = neast().x-swest().x+1;
-	int hh = neast().y-swest().y+1;
-	l_eye = new line(point(swest().x+2,swest().y+hh*3/4),2);
-	r_eye = new line(point(swest().x+ll-4,swest().y+hh*3/4),2);
-	mouth = new line(point(swest().x+2,swest().y+hh/4),ll-4);
+  int ll = neast().x-swest().x+1;
+  int hh = neast().y-swest().y+1;
+  l_eye = new line(point(swest().x+2,swest().y+hh*3/4),2);
+  r_eye = new line(point(swest().x+ll-4,swest().y+hh*3/4),2);
+  mouth = new line(point(swest().x+2,swest().y+hh/4),ll-4);
 }
 void myshape::draw()
 {
-	rectangle::draw();
-	int a = (swest().x+neast().x)/2;
-	int b = (swest().y+neast().y)/2;
-	put_point(point(a,b));
+  rectangle::draw();
+  int a = (swest().x+neast().x)/2;
+  int b = (swest().y+neast().y)/2;
+  put_point(point(a,b));
 }
 void myshape::move(int a, int b)
 {
-	rectangle::move(a,b);
-	l_eye->move(a,b);
-	r_eye->move(a,b);
-	mouth->move(a,b);
+  rectangle::move(a,b);
+  l_eye->move(a,b);
+  r_eye->move(a,b);
+  mouth->move(a,b);
 }
 
-//================================
 int main( )
 {   setlocale(LC_ALL, "ru-RU");
-	screen_init();
-	//1.Объявление набора фигур
-	rotatable* p1 = new rectangle(point(0,0),point(14,5));// цилиндр
-	shape* p2 = new line(point(0,15),17); //ободок цилиндра
-	shape* p3 = new myshape(point(25,10),point(37,18)); //лицо
-	square_sidelong_cross *p4 = new square_sidelong_cross(point(0,0),6);
-	square_sidelong_cross *p5 = new square_sidelong_cross(point(40,0), 6);
-	square_sidelong_cross *p6 = new square_sidelong_cross(point(0,0), 6);
+  screen_init();
+  //1.Объявление набора фигур	
+  rotatable* p1 = new rectangle(point(0,0),point(16,5));// цилиндр
+  shape* p2 = new line(point(0,15),17); //ободок цилиндра
+  shape* p3 = new myshape(point(25,10),point(37,18)); //лицо
+  square_sidelong_cross *p4 = new square_sidelong_cross(point(0,0),6);//по бокам
+  square_sidelong_cross *p5 = new square_sidelong_cross(point(0,0), 6);
+  square_sidelong_cross *p6 = new square_sidelong_cross(point(0,0),6);//эмблема  
 
+  //2.Ориентация
+  p1->rotate_right();
+	   	
+  //3.Сборка изображения
+  p3->move(-10,-10);
+  stack(p2,p3);
+  stack(p1,p2);
+  l_side(p4,p2);
+  r_side(p5,p2);
+  put_to_center(p6, p1);
+  shape_refresh();
 
-	  
-			//	_getch();
-//2.Ориентация
-	  p1->rotate_right();
-	   
-	  //  shape_refresh();
-	//	_getch();
-//3.Сборка изображения
-	  p3->move(-10,-10);
-	  stack(p2,p3);
-	  stack(p1,p2);
-	  l_side(p4,p2);
-	  r_side(p5,p2);
-	  put_to_center(p6, p1);
-	  shape_refresh();
-
-
-	//	_getch();
-	//	screen_destroy();
-	return 0;
+  return 0;
 }
--- hierarchy/sidelong_cross.h
+++ hierarchy/sidelong_cross.h
@@ -2,7 +2,7 @@
 //Крест
 class cross : virtual public shape {
   point c; int w;
-public:
+ public:
   point north() const { return point(c.x,c.y+w); }
   point south() const { return point(c.x,c.y-w); }
   point east() const { return point(c.x-w,c.y); }
@@ -12,68 +12,44 @@ public:
   point nwest() const { return point(c.x-w, c.y+w); }
   point swest() const { return point(c.x-w,c.y-w); }
   void move(int a, int b)
-	{ c.x+=a; c.y+=b;}
-  virtual void draw();
+  { c.x+=a; c.y+=b;}
+  void draw();
   cross(point,int);
 };
+
 cross::cross(point a, int b) : c(a), w(b)
-{ }  
+{ }
+
 void cross::draw()
 {
   put_line(north(),south());
   put_line(east(),west());
 }
 
-//Прямоугольник с крестом
-class r_cross : public rectangle, public cross {
-public:
-  point north() const { return cross::north(); }
-  point south() const { return cross::south(); }
-  point east() const { return cross::east(); }
-  point west() const { return cross::west(); }
-  point neast() const { return rectangle::neast(); }
-  point seast() const { return rectangle::seast(); }
-  point nwest() const { return rectangle::nwest(); }
-  point swest() const { return rectangle::swest(); }
-  void move(int a, int b)
-	{ rectangle::move(a, b); cross::move(a, b); }
-  void draw();
-  r_cross(point,point,int);
-};
-r_cross::r_cross(point a, point b, int c) : rectangle(a, b), cross(point((a.x+b.x)/2, (a.y+b.y)/2), c)
-{ }
-
-void r_cross::draw()
-{
-  rectangle::draw();
-  cross::draw();
-}
-
 // Косой крест
 class sidelong_cross : public cross {
-public:
+ public:
   void draw()
   {
     put_line(nwest(),seast());
     put_line(swest(),neast());
   }
-  sidelong_cross(point a, int b) : cross(a,b)
-{ } 
+ sidelong_cross(point a, int b) : cross(a,b) { } 
 };
 
 //Прямоугольник с косым крестом
 class r_sidelong_cross : public rectangle, public sidelong_cross {
-public:
-  point north() const { return sidelong_cross::north(); }
-  point south() const { return sidelong_cross::south(); }
-  point east() const { return sidelong_cross::east(); }
-  point west() const { return sidelong_cross::west(); }
-  point neast() const { return rectangle::neast(); }
-  point seast() const { return rectangle::seast(); }
-  point nwest() const { return rectangle::nwest(); }
-  point swest() const { return rectangle::swest(); }
+ public:
+  point north() const { return rectangle::north(); }
+  point south() const { return rectangle::south(); }
+  point east() const { return rectangle::east(); }
+  point west() const { return rectangle::west(); }
+  point neast() const { return sidelong_cross::neast(); }
+  point seast() const { return sidelong_cross::seast(); }
+  point nwest() const { return sidelong_cross::nwest(); }
+  point swest() const { return sidelong_cross::swest(); }
   void move(int a, int b)
-	{ rectangle::move(a, b); cross::move(a, b); }
+  { rectangle::move(a, b); sidelong_cross::move(a, b); }
   void draw() {
     rectangle::draw();
     sidelong_cross::draw();
@@ -91,4 +67,4 @@ class square_sidelong_cross : public r_sidelong_cross {
 };
 
 square_sidelong_cross::square_sidelong_cross(point sw, int side) : r_sidelong_cross (sw, point(sw.x+side, sw.y+side), side/2)
-{}
+{ }
